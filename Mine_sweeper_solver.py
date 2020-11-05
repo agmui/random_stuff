ac1b39e858d219ac5e81e643dbd97a49c7a17bd0
Anthony <anthonynmui@gmail.com>
2020-11-05 08:59:24 -0800

    Added Mine sweeper

---
+++ Mine_sweeper_solver.py
@@ -0,0 +1,11 @@
+# 0 is unknown, -1 is non space
+board = [[0] * 8 for i in range(8)]
+
+for i in range(len(board)):
+    print(board[i])
+
+# idk clicked somewhere
+
+
+for i in range(len(board)):
+    print(board[i])
