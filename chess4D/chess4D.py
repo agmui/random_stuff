82ad9b0106ac9e4c940b4fa1fcceea1f72f1332b
agmui <65274668+agmui@users.noreply.github.com>
2020-12-17 17:54:16 -0800

    made chess4D UI and add other pieces

--- chess4D/chess4D.py
+++ chess4D/chess4D.py
@@ -1,54 +1,165 @@
 import pygame as py
-import sys
 import time
-from pygame.locals import *
 
-sys.path.insert(1, r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D')
-import chess4d_code
+# from pygame.locals import *
 
 py.init()
+py.display.set_caption('chess')
 clock = py.time.Clock()  # FPS stuff
 size = 50
-GD = py.display.set_mode((size * 8, size * 8))
+GD = py.display.set_mode((size * 8 + 200, size * 8))
 crashed = False
+select = 0
 
-Bking_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\King.png')
-Bqueen_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Queen.png')
+Bking_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Bking.png')
+Bqueen_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Bqueen.png')
+Bbishop_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Bbishop.png')
+Bknight_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Bknight.png')
+Brook_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Brook.png')
+Bpawn_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Bpawn.png')
+Wking_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Wking.png')
+Wqueen_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Wqueen.png')
+Wbishop_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Wbishop.png')
+Wknight_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Wknight.png')
+Wrook_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Wrook.png')
+Wpawn_pic = py.image.load(r'C:\Users\antho\Documents\GitHub\random_stuff\chess4D\assets\Wpawn.png')
+
+
+# Bking_pic = py.image.load('/Users/Anthony/PycharmProjects/random_stuff/chess4D/assets/King.png')  # .convert_alpha()
+# Bqueen_pic = py.image.load('/Users/Anthony/PycharmProjects/random_stuff/chess4D/assets/Queen.png')  # .convert_alpha()
+
+# sprite_list = [Bking_pic, Bqueen_pic, Bbishop_pic, Bknight_pic, Brook_pic, Bpawn_pic, Wking_pic, Wqueen_pic,
+#               Wbishop_pic, Wknight_pic, Wrook_pic, Wpawn_pic]
+
+
+class piece:
+    def __init__(self, pos, type, img):
+        self.pos = pos
+        self.color, self.type = type
+        self.img = img
+        self.size = self.img.get_size()
+
+    def draw(self, x=0):
+        sprite = py.transform.scale(self.img, (size - 5, size - 5))
+        GD.blit(sprite, (self.pos[0] + 3, self.pos[1] + 3))
+
+    def move(self, direction, amount=1):
+        if self.type == 'p':
+            pass
+        elif self.type == 'k':
+            pass
+        elif self.type == 'b':
+            pass
+        elif self.type == 'r':
+            pass
+        elif self.type == 'q':
+            if direction == 'U':
+                self.pos = (self.pos[0], self.pos[1] - 50)
+        elif self.type == 'King':
+            if direction[0] == 1:
+                self.pos = (self.pos[0] + 50, self.pos[1])
+            elif direction[0] == -1:
+                self.pos = (self.pos[0] - 50, self.pos[1])
+            if direction[1] == 1:
+                self.pos = (self.pos[0], self.pos[1] + 50)
+            elif direction[1] == -1:
+                self.pos = (self.pos[0], self.pos[1] - 50)
+
+    def move_check(self, mx, my):
+        if self.type == 'p':
+            pass
+        elif self.type == 'k':
+            pass
+        elif self.type == 'b':
+            pass
+        elif self.type == 'r':
+            pass
+        elif self.type == 'q':
+            return 1, 1
+        elif self.type == 'King':
+            if abs(int(mx / 50) - int(self.pos[0] / 50)) <= 1 and abs(int(my / 50) - int(self.pos[1] / 50)) <= 1:
+                return int(mx / 50) - int(self.pos[0] / 50), int(my / 50) - int(self.pos[1] / 50)
 
 
 def init_board():
     for i in range(8):
         for j in range(8):
             if (i + j) % 2 == 0:
-                c = (201, 123, 58)
-            else:
                 c = (227, 187, 154)
-            py.draw.rect(GD, c, (j * size, i * size, size, size))
-    # set up chess pieces
-    Bking = chess4d_code.piece((size * 4, size * 4), 'King', Bking_pic)  # fine tune
-    Bqueen = chess4d_code.piece((size * 4, size * 4), 'King', Bqueen_pic)
-    return Bking, Bqueen
-
-
-while not crashed:  # makes window not buggy
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            crashed = True
-        elif event.type == MOUSEBUTTONDOWN:
-            mx, my = py.mouse.get_pos()
-            px, py = Bking.get_pos()
-            if mx >= px and my >= px and mx <= px+50 and my <= px+50:
-                print('on')
             else:
-                print('off')
+                c = (201, 123, 58)
+            py.draw.rect(GD, c, (j * size, i * size, size, size))
+    for pieces_ in p:
+        pieces_.draw()
 
-    Bking, Bqueen = init_board()
 
-    Bking.move("U")
-    Bking.draw1()
+def UI():
+    py.draw.rect(GD, (255, 255, 255), (size * 8, 0, 200, size * 8))
+    font = py.font.Font('freesansbold.ttf', 32)
+    text = font.render('Moves:', True, (0, 0, 0))
+    GD.blit(text, (size * 8 + 10, 0))
 
-    Bqueen.draw1()
 
-    py.display.update()
-    clock.tick(60)
-py.quit()
+# set up chess pieces
+Bking = piece((size * 4, 0), ('B', 'King'), Bking_pic)
+Bqueen = piece((size * 3, 0), ('B', 'King'), Bqueen_pic)
+Bbishop1 = piece((size * 2, 0), ('B', 'King'), Bbishop_pic)
+Bknight1 = piece((size * 1, 0), ('B', 'King'), Bknight_pic)
+Brook1 = piece((size * 0, 0), ('B', 'King'), Brook_pic)
+Bbishop2 = piece((size * 5, 0), ('B', 'King'), Bbishop_pic)
+Bknight2 = piece((size * 6, 0), ('B', 'King'), Bknight_pic)
+Brook2 = piece((size * 7, 0), ('B', 'King'), Brook_pic)
+Bpawn1 = piece((size * 0, size * 1), ('B', 'King'), Bpawn_pic)
+Wking = piece((size * 4, size * 7), ('W', 'King'), Wking_pic)
+Wqueen = piece((size * 3, size * 7), ('W', 'King'), Wqueen_pic)
+Wbishop1 = piece((size * 2, size * 7), ('W', 'King'), Wbishop_pic)
+Wknight1 = piece((size * 1, size * 7), ('W', 'King'), Wknight_pic)
+Wrook1 = piece((size * 0, size * 7), ('W', 'King'), Wrook_pic)
+Wbishop2 = piece((size * 5, size * 7), ('W', 'King'), Wbishop_pic)
+Wknight2 = piece((size * 6, size * 7), ('W', 'King'), Wknight_pic)
+Wrook2 = piece((size * 7, size * 7), ('W', 'King'), Wrook_pic)
+Wpawn1 = piece((size * 0, size * 6), ('W', 'King'), Wpawn_pic)
+
+# list of all pieces
+p = [Bking, Bqueen, Bbishop1, Bknight1, Brook1, Bbishop2, Bknight2, Brook2, Bpawn1, Wking, Wqueen, Wbishop1, Wknight1,
+     Wrook1, Wbishop2, Wknight2, Wrook2, Wpawn1]
+
+
+def main():
+    global py, crashed, size, select
+    while not crashed:  # makes window not buggy
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                crashed = True
+            elif event.type == py.MOUSEBUTTONDOWN:
+                mx, my = py.mouse.get_pos()
+
+                for pieces_ in p:
+                    x, y = pieces_.pos
+                    if x <= mx <= x + 50 and y <= my <= y + 50:
+                        print('on', pieces_.type)
+                        select = pieces_
+                        break
+                    else:
+                        print('off', pieces_.type)
+                        if select != 0 and select.move_check(mx, my):  # check if possible pos
+                            # direction, amount = select.move_check(mx, my)
+                            # print(direction, amount)
+                            select.move(select.move_check(mx, my))
+                            break
+                        else:
+                            select = 0
+
+                if select != 0:
+                    print('selected', select.type)  # ts
+                else:
+                    print('selected none')
+        init_board()
+        UI()
+
+        py.display.update()
+        clock.tick(60)
+    py.quit()
+
+
+main()
