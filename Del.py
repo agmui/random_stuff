ab17ff8bd6fd5822de0353d6cdee49fff28212f6
Anthony <anthonynmui@gmail.com>
2020-12-28 22:50:33 -0800

    Made some physics

--- Del.py
+++ Del.py
@@ -1,32 +1,21 @@
-class myc:
-    def __init__(self):
-        self.x = 0
-    def add(self,y):
-        self.x += y
+"""import pygame
 
-import pygame
 
 pygame.init()
 clock = pygame.time.Clock()  # FPS stuff
-GD = pygame.display.set_mode((50, 50))
 crashed = False
-hi = False
+
 while not crashed:  # makes window not buggy
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                n.add(2)
-                hi = True
-                print(n.x)
-        GD.fill((110, 110, 110))  # draw background
-        n = myc()
-        if hi:
-            n.add(10)
 
 
-    pygame.display.update()
+
     clock.tick(60)
 
-pygame.quit()
+pygame.quit()"""
+
+import math
+
+print(math.cos(90*(math.pi/180)))
--- LM_game/LM_game.py
+++ LM_game/LM_game.py
@@ -1,3 +1,4 @@
+import math
 import os
 import pygame as py
 
@@ -6,49 +7,68 @@ py.display.set_caption('LM game')
 clock = py.time.Clock()  # FPS stuff
 GD = py.display.set_mode((500, 500))
 crashed = False
+fire = False
+t = 1 / 60
+thrust = 20
 
 LM = py.image.load(os.path.join("assets", "LM.png"))
 
 
 class Player:
     def __init__(self, fuel):
-        self.pos = [0, 0]
-        self.angle = 0
-        self.v = 0
-        self.acc = [0, 0]
+        self.pos = [100, 100]
+        self.angle = 90
+        self.v = [0, 0]
+        self.acc = [0, 9.81]# has to be not negative cuz py window
         self.fuel = fuel
         self.LM = LM
 
-    def draw(self):
-        img_copy = py.transform.rotate(self.LM, - self.angle)
-        GD.blit(img_copy, (self.pos[0] - int(img_copy.get_width() / 2), self.pos[1] - int(img_copy.get_height() / 2)))
-        # GD.blit(py.transform.scale(LM, (size, size)), (self.pos[0], self.pos[1]))
-
-    def math(self):
-        pass
+    def draw(self, fire_, scale=0.2):
+        img = py.transform.rotozoom(self.LM, self.angle - 90, scale)
+        GD.blit(img, (self.pos[0] - int(img.get_width() / 2), self.pos[1] - int(img.get_height() / 2)))
+        if fire_:
+            py.draw.rect(GD, (255, 0, 0), py.Rect(self.pos[0]-10, self.pos[1]+10, 20, 20))
 
     def tilt(self, x):
         self.angle += x
 
+    def physics(self):
+        for i in range(2):
+            self.v[i] = self.v[i] + self.acc[i] * t
+            self.pos[i] = self.pos[i] + self.v[i] * t + 0.5 * self.acc[i] * t ** 2
+
+    def rocket(self):
+        self.acc[0] = -thrust*math.cos(self.angle*(math.pi/180))
+        self.acc[1] = 9.81-thrust*math.sin(self.angle*(math.pi/180))
+
+
 player = Player(100)
 
+
 def main():
-    global crashed
+    global crashed, fire
     while not crashed:  # makes window not buggy
         for event in py.event.get():
             if event.type == py.QUIT:
                 crashed = True
-            if event.type == py.KEYDOWN:
-                if event.key == py.K_LEFT:
-                    player.tilt(10)
-                elif event.key == py.K_RIGHT:
-                    player.tilt(-10)
-                elif event.key == py.K_DOWN:
-                    pass
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_LEFT:
+                player.tilt(1)
+            elif event.key == py.K_RIGHT:
+                player.tilt(-1)
+            elif event.key == py.K_DOWN:
+                player.rocket()
+                fire = True
+        if event.type == py.KEYUP:
+            if event.key == py.K_DOWN:
+                player.acc = [0, 9.81]
+                fire = False
+
 
         GD.fill((255, 255, 255))  # draw background
 
-        player.draw()
+        player.physics()
+        player.draw(fire)
 
         py.display.update()
         clock.tick(60)
