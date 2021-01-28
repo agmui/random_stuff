6d4cc108236b213ddffd343247ccc8eecb9ea5dd
agmui <65274668+agmui@users.noreply.github.com>
2021-01-28 01:59:55 -0800

    started working on better birbs

--- birbs/better_birbs/birb.py
+++ birbs/better_birbs/birb.py
@@ -1,4 +1,8 @@
+import math
+
+num_of_birbs = 2
 birb_list = []
+poslist = []
 
 
 class birb:
@@ -6,18 +10,74 @@ class birb:
         self.pos = pos
         self.angle = angle
         self.speed = 10
+        self.nearByBirbsList = []
 
-    def someMoveFunction(self):#ts
-        self.pos = [self.pos[0]+10, self.pos[1]]
+    def path_finding(self):
+        self.sight()
+        self.avoid_filter()
+        self.same_direction_filter()
+        self.center_dive()
+        #self.move()
+
+    def sight(self):
+        for i in poslist:
+            dx, dy = self.pos[0] - i[0], i[1] - self.pos[1]
+            distance = math.hypot(dx, dy)
+            angle = math.degrees(math.atan2(dy, dx))*-1
+            if 0 != distance <= 100 and not (-90 + 10 >= angle >= -90 - 10):
+                self.nearByBirbsList.append([distance, angle])
+                #if self.pos == [150, 100]:
+                #    print(angle)
+
+    def avoid_filter(self):
+        total = 0
+        for i in self.nearByBirbsList:
+            distance, angle = i[0], i[1]
+            total -= math.exp(0.1*distance-1) * angle
+        self.angle += total
+
+    def same_direction_filter(self):
+        pass
+
+    def center_dive(self):
+        pass
+
+    def move(self):
+        r = 1
+        self.pos[0] += r * math.cos(self.angle)  # make sure it is in radians or something
+        self.pos[1] += r * math.sin(self.angle)
 
     def getPos(self):
         return self.pos
 
-
-def main():
-    for i in range(5):
-        b = birb([0, 0], 0)
-        birb_list.append(b)
+    def getAngle(self):
+        return self.angle
 
 
-main()
+def getAllBirbPos():
+    poslist.clear()
+    for i in birb_list:
+        poslist.append(i.getPos())
+
+
+def init():
+    # for i in range(num_of_birbs):
+    #    b = birb([0, 0], 0)
+    #    birb_list.append(b)
+    b1 = birb([100, 100], 0)
+    b2 = birb([150, 100], 0)
+    birb_list.append(b1)
+    birb_list.append(b2)
+
+
+def main(Mouse_x, Mouse_y):
+    if Mouse_x != -1:
+        birb_list[0].pos = (Mouse_x, Mouse_y)
+    getAllBirbPos()
+    for i in birb_list:
+        i.path_finding()
+        # i.move()
+
+
+if __name__ == '__main__':
+    main()
--- birbs/better_birbs/window.py
+++ birbs/better_birbs/window.py
@@ -1,46 +1,69 @@
-# use dis as example
-# research about python box2d
-# rect function for pos and size
-# imports image in classes
 import os
+import time
 
 import pygame
+from birbs.better_birbs import birb
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 690
+num_of_birbs = birb.num_of_birbs
+viswals = False
 
-birb = os.path.join("assets", "temp_birb.png")
+birb_pic = os.path.join("assets", "birb.png")
+birb_touch = os.path.join("assets", "birb_touch.png")
+sight = os.path.join("assets", "sight.png")
 
+# pygame.transform.rotozoom(self.image, self.angle, 1)
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, x=50, y=50, idNum=0):
+    def __init__(self, pic, index):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(birb).convert_alpha()
+        self.image = pygame.image.load(pic).convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.image.get_rect(center=(x, y))
-        self.newPos = birb.birb_list[idNum]
+        self.rect = self.image.get_rect(center=(0, 0))
+        self.index = index
 
-    def moveBy(self, distance):
+    def changeColor(self):
+        self.image = pygame.image.load(birb_touch).convert_alpha()
+
+    def moveBy(self, changeInPos):
+        distance = changeInPos[self.index][0] - self.getPos()[0], changeInPos[self.index][1] - self.getPos()[1]
         self.rect.move_ip(distance)
+
+    def rotate(self, birbList):
+        deg = birbList[self.index].getAngle()
+        self.image = pygame.transform.rotate(self.image, deg) # helpp ------------------------------------
 
     def getPos(self):
         return self.rect.center
 
-    def getNewPos(self):
-        return self.newPos
+    def draw(self):
+        window.blit(self.image, self.getPos())
 
-### MAIN
+
+def moveAllBirbs(changeInPos):
+    for i in sprite_group:
+        i.moveBy(changeInPos)
+        if viswals:
+            for j in sight_group:
+                j.moveBy(changeInPos)
+        i.rotate(birb.birb_list)
+
+
+birb.init()
 pygame.init()
 pygame.font.init()
 SURFACE = pygame.HWSURFACE | pygame.DOUBLEBUF
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), SURFACE)
 
-
-
 sprite_group = pygame.sprite.Group()  # All sprites for updating and drawing
-for i in range(5):
-    b = Sprite(0, 0, i)
+sight_group = pygame.sprite.Group()
+for i in range(num_of_birbs):
+    b = Sprite(birb_pic, i)
     sprite_group.add(b)
+    if viswals:
+        s = Sprite(sight, i)
+        sight_group.add(s)
 
 clock = pygame.time.Clock()
 done = False
@@ -49,15 +72,26 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    for i in sprite_group:
-        i.moveBy((i.getNewPos[0] - i.getPos()[0], i.getNewPos[1] - i.getPos()[1])) # birb.py file input
+    Mouse_x, Mouse_y = pygame.mouse.get_pos()
+
+    birb.main(Mouse_x, Mouse_y)
+    changeInPos = birb.poslist
+    moveAllBirbs(changeInPos)
+
 
     # Repaint the screen
     sprite_group.update()  # re-position the game sprites
+    if viswals:
+        sight_group.update()
     window.fill((100, 100, 100))
-    sprite_group.draw(window)  # draw the game sprites
+    # sprite_group.draw(window)  # draw the game sprites
+    for i in sprite_group:
+        i.draw()
+    if viswals:# somehow order sight to back--------------------------------------------
+        sight_group.draw(window)
 
     pygame.display.flip()
     clock.tick_busy_loop(60)
+    time.sleep(0.1)
 
 pygame.quit()
