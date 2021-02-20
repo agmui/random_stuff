d217aa6790cc1efec53d3c1c7e2eef1cddfb01b9
Anthony <anthonynmui@gmail.com>
2021-02-19 23:42:46 -0800

    got webscraper to work

--- webscraping/webscrapingtest.py
+++ webscraping/webscrapingtest.py
@@ -3,7 +3,6 @@ import requests
 import sys
 from lxml import html
 
-
 """sys.stdout = open('output.html', 'wt')
 res = requests.get('https://ahs-fusd-ca.schoolloop.com/portal/login')
 
@@ -18,11 +17,11 @@ page_title = soup.title.text
 #print(page_title)
 """
 
-USERNAME = "Morshu",
-PASSWORD = "kumar4567"
-
+USERNAME = "amui",  # "Morshu",
+PASSWORD = "momand123"  # "kumar4567"
 
 URL = "https://ahs-fusd-ca.schoolloop.com/portal/login?etarget=login_form"
+
 
 def main():
     session = requests.session()
@@ -32,23 +31,41 @@ def main():
     tree = html.fromstring(result.text)
     form_data_id = list(set(tree.xpath("//input[@name='form_data_id']/@value")))[0]
 
-    tree = html.fromstring(result.text)
-    return_url = list(set(tree.xpath("//input[@name='return_url']/@value")))[0]
-
     # Create payload
     payload = {
-        "username": USERNAME,
+        "login_name": USERNAME,
         "password": PASSWORD,
         "form_data_id": form_data_id,
-        "return_url": return_url
+        "event_override": "login"
     }
 
     session.post(URL, payload)
+    session = session.get("https://ahs-fusd-ca.schoolloop.com/portal/student_home")
 
-    session = session.get(URL)
+    #sys.stdout = open('output.html', 'wt')
+    #print(session.text)
 
-    sys.stdout = open('output.html', 'wt')
-    print(session.text)
+    soup = BeautifulSoup(session.content, 'html.parser')
+    output = findTag(soup, 'div', 'class', 'ajax_accordion_row')
+    soup = BeautifulSoup(str(output), 'html.parser')
+    print(output)
+    #print(findTag(soup, 'td', 'class', 'column padding_5'))
+
+def findTag(soup, tag, type, name):
+    _break = False
+    l = ""
+    for i in soup.find_all(tag):
+        # print(i)
+        try:
+            if i.get(type)[0] == name:
+                # print(i)
+                l += i + "\n"
+                _break = True
+        except:
+            pass
+        if _break:
+            pass#break
+    return l
 
 if __name__ == '__main__':
     main()
